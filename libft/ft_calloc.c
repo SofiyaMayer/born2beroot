3e9f46d9ca2d4bb6e73dc2f11c0bf9e0f43a3969
Sofie Meyer <sofiyameyer@hotmail.com>
2025-12-05 18:26:35 +0100

    Edited calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: someyer <someyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 20:02:58 by someyer           #+#    #+#             */
-/*   Updated: 2025/12/03 20:08:47 by someyer          ###   ########.fr       */
+/*   Updated: 2025/12/05 18:25:37 by someyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@
 
 void *ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	i;
-	char	*array;
-	
+	size_t			i;
+	char			*array;
+	char			*alloc;
+	unsigned long	total_size;
 	i = 0;
-	if (nmemb == 0 || size <= 0)
+	if (nmemb <= 0 || size <= 0)
+	{
+		alloc = malloc(1);
+		return (alloc);
+	}
+	total_size = (unsigned long)(nmemb * size);
+	if (nmemb > 65355 || size > 65355)
 		return (NULL);
 	array = malloc(nmemb * size);
 	if (!array)

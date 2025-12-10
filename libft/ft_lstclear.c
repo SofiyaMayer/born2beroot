96b2cd1f89c8c1688409970e01d576cc433bb341
Sofie Meyer <sofiyameyer@hotmail.com>
2025-12-10 16:38:36 +0100

    Edited lstclear.c

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: someyer <someyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 12:22:08 by someyer           #+#    #+#             */
-/*   Updated: 2025/12/10 16:14:29 by someyer          ###   ########.fr       */
+/*   Updated: 2025/12/10 16:38:20 by someyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	while (*lst)
 	{
 		tmp = (*lst)->next;
-		del(lst);
-		free(lst);
-		lst = &tmp;
+		del((*lst)->content);
+		free(*lst);
+		*lst = tmp;
 	}
 	*lst = NULL;
 }
